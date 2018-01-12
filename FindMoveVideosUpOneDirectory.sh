bf83a3bdbacd28757025f46b0d3ac00fb5bc9956
char00les <char00les@gmail.com>
2018-01-11 22:11:46 -0500

    Create FindMoveVideosUpOneDirectory.sh

---
+++ FindMoveVideosUpOneDirectory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+find -name *.wmv -exec mv '{}' .. \;
+find -name *.avi -exec mv '{}' .. \;
+find -name *.AVI -exec mv '{}' .. \;
+find -name *.mp4 -exec mv '{}' .. \;
+find -name *.mpg -exec mv '{}' .. \;
+find -name *.flv -exec mv '{}' .. \;
+find -name *.FLV -exec mv '{}' .. \;
